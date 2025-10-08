fac7fc96c84f6f76907f65d5cce36354c6df41f4
Rei Hakurei <contact@bloom-juery.net>
2025-10-08 19:00:34 +0700

    [doc] clarify better explanation on refine-related modules

--- lib/maimai_net/core_ext.rb
+++ lib/maimai_net/core_ext.rb
@@ -1,5 +1,9 @@
 module MaimaiNet
   module CoreExt
+    # contains every {Constants AutoConstant} classes turned into a function.
+    # used for include or prepend in a scope.
+    #
+    # also used in `refine` {IncludeAutoConstant}.
     module AutoConstantInclusion
       MaimaiNet.constants.each do |k|
         cls = MaimaiNet.const_get(k)
